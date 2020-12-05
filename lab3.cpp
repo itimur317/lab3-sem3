cfd6633037e4d891fd3ce73308c6cf6d9a9ab6c3
itimur317 <56135499+itimur317@users.noreply.github.com>
2020-12-05 15:23:52 +0300

    Add files via upload

--- lab3.cpp
+++ lab3.cpp
@@ -40,37 +40,58 @@ public:
         }
     }
 private:
-    int min_path(int* short_dist, bool* visited_vertexes) {
+    int min_path(Sequence<int>* short_dist, Sequence<bool>* visited_vertexes) {
         int min_index;
         int min = INT_MAX;
         for (int i = 0; i < n; i++) {
-            if (!visited_vertexes[i] && (short_dist[i] <= min)) {
-                min = short_dist[i];// from visited take minimum
+            if (!visited_vertexes->Get(i) && (short_dist->Get(i) <= min)) {
+                min = short_dist->Get(i);// from visited take minimum
                 min_index = i;
             }
         } 
         return min_index;
     }
 
+    Sequence<int>* find_path(Sequence<int>* past_vertexes_path,
+        Sequence<int>* short_dist,
+        int start_vertex, int end_vertex) {
+        Sequence<int>* finded_path = new ArraySequence<int>;
+        int cur = end_vertex;
+        finded_path->Append(cur);
+        while (cur != start_vertex) {
+            if (short_dist->Get(cur) == INT_MAX) {
+                finded_path = new ArraySequence<int>;
+                return finded_path;
+            }
+            cur = past_vertexes_path->Get(cur);
+            finded_path->Prepend(cur);
+        }
+        return finded_path;
+    }
+
 public:
-    int* Dijkstra(int start_vertex, int end_vertex) {
+    Sequence<int> * Dijkstra(int start_vertex, int end_vertex) {
         if (start_vertex < 0 || (end_vertex > n - 1) && (n > 1)
             || start_vertex > end_vertex) 
             throw out_of_range(IndexOutOfRangeEx);
         // just check edges
 
-        int* short_dist = new int[n];
-        bool* visited_vertexes = new bool[n];
-        Sequence<int>* path = new ArraySequence<int>;
+        Sequence<int>* short_dist = new ArraySequence<int>(n);
+        Sequence<bool>* visited_vertexes = new ArraySequence<bool>(n);
+        Sequence<int>* past_vertexes_path = new ArraySequence<int>(n);
 
         for (int i = 0; i < n; i++) {
-            short_dist[i] = INT_MAX; 
-            visited_vertexes[i] = 0;
+            short_dist->Set(i, INT_MAX);
+            visited_vertexes->Set(i, 0);
         }
 
-        short_dist[start_vertex] = 0;
+        short_dist->Set(start_vertex, 0);
 
-        
+        // fill seq path past vertexes, for start it`s start_vertex
+        for (int i = 0; i < n; i++) {
+            past_vertexes_path->Set(i, start_vertex);
+        }
+
 
         for (int i = 0; i + 1 < n; i++) {
             // cycle n - 1 times
@@ -79,24 +100,28 @@ public:
             int min = min_path(short_dist, visited_vertexes);
             // min`s vertexes isn`t free anymore 
             // min - index of the shortest path
-            visited_vertexes[min] = 1;
+            visited_vertexes->Set(min, 1);
 
             for (int j = 0; j < n; j++) {
-                if (!visited_vertexes[j] &&
+                if (!visited_vertexes->Get(j) &&
                     // it wasn`t visited
                     (graph->Get(j + min * n)) &&
                     // it isn`t zero
-                    (short_dist[min] != INT_MAX) &&
+                    (short_dist->Get(min) != INT_MAX) &&
                     // if it = INT_MAX then it illogical
-                    (short_dist[min] + graph->Get(j + min * n) < short_dist[j])) {
+                    (short_dist->Get(min) + graph->Get(j + min * n) < short_dist->Get(j))) {
                     // if this path less than past
-                    
-                    short_dist[j] = short_dist[min] + graph->Get(j + min * n);
+                    // this if for fill path
+                    past_vertexes_path->Set(j, min);
+                    // this if to find distance
+                    short_dist->Set(j, short_dist->Get(min) + graph->Get(j + min * n));
                 }
             }
         }
 
-        return short_dist;
+
+
+        return find_path(past_vertexes_path, short_dist, start_vertex, end_vertex);
     }
 };
 
@@ -125,8 +150,9 @@ int main()
     Graph* current = new Graph(a, 5);
     current->print_matrix();
     cout << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << (current->Dijkstra(1, 3))[i] << "  ";
+    Sequence<int>* dick = current->Dijkstra(1, 4);
+    for (int i = 0; i < dick->GetLength(); i++) {
+        cout << dick->Get(i) << "  ";
     }
     return 0;
 }
